8e0330766e3c62052b88638779ea924a79169a15
Michael Foessinger <michael.foessinger@hotmail.com>
2024-06-16 14:17:41 +0200

    fixed bugs in routing, naming etc. (URLS & Templates)

--- techmax/shop/views.py
+++ techmax/shop/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def shop(request):
     return render(request, 'shop/shop.html')
-def shop(request):
+def warenkorb(request):
     return render(request, 'shop/warenkorb.html')
-def shop(request):
+def kasse(request):
     return render(request, 'shop/kasse.html')
