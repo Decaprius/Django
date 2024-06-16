ea3e78a091a0d9cd76935ad3aeab67c0d80b7e32
Michael Foessinger <michael.foessinger@hotmail.com>
2024-06-16 13:58:00 +0200

    implemented basic urls (shop, warenkorb, kasse)

--- techmax/shop/views.py
+++ techmax/shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def shop(request):
+    return render(request, 'shop/shop.html')
+def shop(request):
+    return render(request, 'shop/warenkorb.html')
+def shop(request):
+    return render(request, 'shop/kasse.html')
